35c1cb63c94988fc5d6b323f4c67ce83f6d0bde8
start.vaadin.com <no-reply@vaadin.com>
2023-02-11 18:52:46 +0000

    Generated project

---
+++ src/main/java/it/trekkete/views/demo/DemoView.java
@@ -0,0 +1,33 @@
+package it.trekkete.views.demo;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import it.trekkete.views.MainLayout;
+
+@PageTitle("Demo")
+@Route(value = "demo", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class DemoView extends VerticalLayout {
+
+    public DemoView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
